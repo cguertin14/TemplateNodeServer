087a8de3b34fdc6676a5688efe168e38ef4e49fc
Charles Guertin <charlesguertin@live.ca>
2018-04-13 22:50:41 -0400

    Initial content

---
+++ server/server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+
+// Application (express).
+const app = express();
+// Port of the server.
+const port = process.env.PORT || 3000;
+
+
+/*
+
+    ROUTES GO HERE.
+
+*/
+
+
+server.listen(port, () => {
+    console.log(`Server listening on port ${port}`)
+});
